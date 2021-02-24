ba4820cc1a451bc0b6444ef110db138cb897429f
solipkimm@gmail.com <59662367+solipkimm@users.noreply.github.com>
2021-02-24 16:51:59 -0500

    add form

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [accountType, setAccountType] = useState('Student')
+
+  const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log('The Form Was Submitted: ' + 
+        JSON.stringify(username) + 
+        ' ' + 
+        JSON.stringify(accountType) )
+        setUsername("")
+        setAccountType('Student')
+  };
+
+  const onUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const onAccountTypeChange = (e) => {
+    setAccountType(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <form onSubmit={handleSubmit}>
+        <label>
+          User Name: 
+          <input type="text" name="username" value={username} onChange={onUsernameChange} />
+        </label><br />
+        <label>
+          Accout Type: 
+          <select name="accountType" value={accountType} onChange={onAccountTypeChange}>
+            <option value="Student">Student</option>
+            <option value="Employee">Employee</option>
+            <option value="Admin">Admin</option>
+          </select>
+        </label><br />
+        <button type="submit">Submit</button>
+    </form>
+  )
 }
 
 export default App;
